05f816504a308e761acb37b53333d89652990835
Travis Tressler <travis.tressler@interapt.com>
2017-10-24 19:33:38 -0400

    [MAP] map is displayed.

--- app/src/main/java/com/example/travistressler/memories/MemoryMapFragment/MemoryMapFragment.java
+++ app/src/main/java/com/example/travistressler/memories/MemoryMapFragment/MemoryMapFragment.java
@@ -1,14 +1,27 @@
 package com.example.travistressler.memories.MemoryMapFragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.travistressler.memories.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -18,6 +31,9 @@ import butterknife.ButterKnife;
 public class MemoryMapFragment extends Fragment implements MemoryMapView {
 
     private MemoryMapPresenter presenter;
+    @BindView(R.id.map_view)
+    public MapView mapView;
+
 
     @Nullable
     @Override
@@ -26,6 +42,36 @@ public class MemoryMapFragment extends Fragment implements MemoryMapView {
         ButterKnife.bind(this, view);
         presenter = new MemoryMapPresenter();
         presenter.attachView(this);
+
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                googleMap.setMyLocationEnabled(true);
+
+                try {
+                    MapsInitializer.initialize(MemoryMapFragment.this.getActivity());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // Updates the location and zoom of the MapView
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 5);
+                googleMap.animateCamera(cameraUpdate);
+            }
+        });
+
         return view;
     }
 
@@ -36,5 +82,23 @@ public class MemoryMapFragment extends Fragment implements MemoryMapView {
         MemoryMapFragment fragment = new MemoryMapFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
