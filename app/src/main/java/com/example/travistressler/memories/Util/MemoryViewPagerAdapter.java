869e0435abbe379c4648afa71a40a37c38a54d32
Travis Tressler <travis.tressler@interapt.com>
2017-10-24 12:45:34 -0400

    [PAGER ADAPTER] logic finished.

---
+++ app/src/main/java/com/example/travistressler/memories/Util/MemoryViewPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.example.travistressler.memories.Util;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.travistressler.memories.AddMemoryFragment.AddMemoryFragment;
+import com.example.travistressler.memories.MemoryGridFragment.MemoryGridFragment;
+import com.example.travistressler.memories.MemoryMapFragment.MemoryMapFragment;
+
+import java.util.List;
+
+/**
+ * Created by travistressler on 10/24/17.
+ */
+
+public class MemoryViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public MemoryViewPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return AddMemoryFragment.newInstance();
+            case 1:
+                return MemoryMapFragment.newInstance();
+            case 2:
+                return MemoryGridFragment.newInstance();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
